a87750fe45fbdf6ad35a341aca8a7e2a3fd6446d
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-05-06 17:20:30 +0200

    made gcd() functional

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,32 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <cmath>
+
+int gcd(int a, int b)
+{
+	int smallerNumber = a;
+	int biggerNumber = b;
+
+	if (a > b) {
+		biggerNumber = a;
+	       	smallerNumber = b;
+	}
+
+	int res = smallerNumber;
+	while (((biggerNumber % res) + (smallerNumber % res)) != 0) {
+		res--;
+		}
+	return res;
+}
+
+TEST_CASE("describe_gcd", "[gcd]")
+{
+	REQUIRE(gcd(2,4) == 2);
+	REQUIRE(gcd(9,6) == 3);
+	REQUIRE(gcd(3,7) == 1);
+}
 
 int main(int argc, char* argv[])
 {
-  return Catch::Session().run(argc, argv);
+	return Catch::Session().run( argc, argv );
 }
